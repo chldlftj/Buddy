bc4512800ef5949bfb3932fdefea9b7b736fdef7
choi ilseo <rodeo25229203@gmail.com>
2026-04-27 16:21:12 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String args[]) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[2]);
+        String operator = args[1];
+
+        int result  = switch(operator) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> -1;
+        };
+        System.out.println("result = " + result);
+
+
+    }
+}
